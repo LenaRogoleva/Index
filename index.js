038762d339dea00c2042193302bd2c3f2d041735
s1nkara <smartovickiy@gmail.com>
2021-05-29 15:26:29 +0300

    info(index.js): комменарии по коду

--- index.js
+++ index.js
@@ -1,96 +1,96 @@
 const addButton = document.getElementById('add');
 const inputTask = document.getElementById('new-task');
-const unfinishedTasks = document.getElementById ('unfinished-tasks');
+const unfinishedTasks = document.getElementById('unfinished-tasks');
 const toDoArr = [];
-const toDoArrDelete = []
+const toDoArrDelete = [];
+let todoArrFiltered = [];
 const label = document.createElement('label');
 const priority = document.getElementById('prioritet');
-//todo следить за объявлением переменных, использовать const для значений, которые не меняются, let для всех остальных
-//todo небольшая подсказка на будущее: массив ты меняешь, только если заново перезаписываешь его значение,
-// то есть меняешь ссылку, например todoArr = [1,2,3] - массив перезаписа, todoArr.splice(0,1) - массив(то есть его ссылка)
-// осталась прежней, просто изменилось его содержание, соответственно у тебя в коде пока что массив todoArr - это const
-//todo колдуй над кодом дальше и всё получится:)
+//todo для id всё же лучше использовать counter
+let counter = 0;
 
 function addTask() {
-    if (inputTask.value === ""){
-        return  alert ('Введите данные')
+    if (inputTask.value === "") {
+        return alert('Введите данные')
     }
     const task = document.getElementById('new-task')
-    //todo использовать id для дальнейшей работы с html и массивом
     let toDo = {
         name: task.value,
         prior: priority.value,
         time: new Date().toLocaleString(),
-        id: toDoArr.length
-    }
-    toDoArr.unshift (toDo)
+        id: ++counter,
+        isVisible: true
+    };
+    toDoArr.unshift(toDo)
 }
 
 
-function set(){
+function set() {
 
-    inputTask.value="" //обнулим значение строки
-    let displayTask = ''
+    inputTask.value = "";//обнулим значение строки
+    let displayTask = '';
     // let task = document.createElement('li')
     // task.id = toDoArr[0].id
     // const unfTasks = document.getElementById('unfinished-tasks')
     // const a = document.getElementById('${item.id}')
     // unfTasks.removeChild(a)
     toDoArr.forEach(item => { //выводим элементы
-        swap(item)
-        //todo использовать привязки событий к конкретному тегу
-        //todo использовать id при удалении и редактировании(подсказка)
+        swap(item);
+        //todo привязываем контекст через this
         displayTask += `<li id ="${item.id}" class="tasks">${prior}
         <label>${item.name}</label>
         <label>${item.time}</label>
-        <i onclick="deleteTask(item.id)" class ="material-icons delete">delete</i>
-        </li>`
+        <i id="${item.id}" onclick="deleteTask(this)" class ="material-icons delete">delete</i>
+        </li>`;
         unfinishedTasks.innerHTML = displayTask;
     })
 
 
 }
+
 addButton.addEventListener('click', function () {
     addTask();
     set();
-})
+});
 
 
 function swap(item) {
     if (item.prior === 'low') {
-            prior = '<font color="red">низкий</font>'
-            } else if (item.prior === 'middle') {
-            prior = '<font color="blue">средний</font>'
-            } else {
-            prior = '<font color="orange">высокий</font>'
-            }
-    return prior
+        //todo объяви уже этот prior где-нибудь :)
+        prior = '<font color="red">низкий</font>'
+    } else if (item.prior === 'middle') {
+        prior = '<font color="blue">средний</font>'
+    } else {
+        prior = '<font color="orange">высокий</font>'
     }
-//todo переписать функцию под удаление по id
-//todo как вариант, после удаления, пробегать по массиву и перезаписывать id, после перерисовывать форму заново, как в функции set
+    return prior
+}
 
-function deleteTask(){
-    (event) => {
-        event.preventDefault() //чтобы не включалось действие по умолчанию
-        let toDoArrDelete = toDoArr.slice(0) //копируем элементы в новый массивы
-        const deleteIndex = toDoArr.findIndex((item) => item.id === id)
-        toDoArr.splice(deleteIndex,1);
-        toDoArrDelete.forEach(i => {
-            // if (event.target.closest('.delete')) {
-            //         // if ($('<i class ="material-icons delete">delete</i>').index(event.target)
-            toDoArr.splice(i,1) })//начиная с i позиции удаляем 1 элемент
-        //     }
-        let displayTask = '';
+//todo т.к. у тебя логика немного отличается от например Викиной, т.е. ты вяжешь событие напрямую через шаблон,
+// то придётся идти на небольшие ухищрения, у i задаём id, пробрасываем контекст элемента и уже в нём получаем id
+// можешь открыть консоль и сама посмотреть
+function deleteTask(item) {
+    console.log(item);
+    let toDoArrDelete = toDoArr.slice(0); //копируем элементы в новый массивы
+    const deleteIndex = toDoArr.findIndex((item) => item.id === item.id);
+    toDoArr.splice(deleteIndex, 1);
+    toDoArrDelete.forEach(i => {
+        // if (event.target.closest('.delete')) {
+        //         // if ($('<i class ="material-icons delete">delete</i>').index(event.target)
+        toDoArr.splice(i, 1)
+    }); //начиная с i позиции удаляем 1 элемент
+    //     }
+    let displayTask = '';
 
-        if (toDoArr.length === 0) unfinishedTasks.innerHTML = '' //если массив пустой, то удаляем и из визуала
-//     })
-    }}
+    if (toDoArr.length === 0) unfinishedTasks.innerHTML = '' //если массив пустой, то удаляем и из визуала
+}
 
-document.querySelector('#input2').oninput = function searchTask(){
+document.querySelector('#input2').oninput = function searchTask() {
     let val = this.value.trim(); //получаем значение, которое пользователь вводит внутрь функции, еще обрезаем пробелы у вводимых данных
     //let elasticItems = document.querySelectorAll(unfinishedTasks.innerHTML); //получаем элементы, откуда получаются данные
     let elasticItems = unfinishedTasks.innerHTML;
-    console.log(elasticItems)
+    console.log(toDoArr);
+    console.log(elasticItems);
     // if (val != '') {
     //     elasticItems.forEach(function (elem){ //перебираем все задачи
     //         if (elem.innerText.search(val) ==-1){ //если внутри задач нет совпадений (search возвращает номер подстроки в строке, если есть, то возвращает номер, если нет, то -1)
@@ -100,9 +100,7 @@ document.querySelector('#input2').oninput = function searchTask(){
     //             elem.classList.remove('hide')
     //         }
     //     })
-    }
-
-
+}
 
 
 // function finishTask () {
